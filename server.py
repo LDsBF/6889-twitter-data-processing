d7c2b47ad9181c4bb4c3d62b9310fa8e742c6b39
LDsBF <tw2774@columbia.edu>
2022-04-16 16:18:11 -0400

    upload project

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+
+
+ 
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
